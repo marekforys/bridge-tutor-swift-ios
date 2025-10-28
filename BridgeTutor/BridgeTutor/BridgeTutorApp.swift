144090640af12b160d5dbc49b0e5b45de1e9196d
Marek Forys <mforys73@gmail.com>
2025-10-28 10:26:16 +0100

    feat: First skeleton of Bridge Tutor app
    Signed-off-by: Marek Forys <mforys73@gmail.com>

---
+++ BridgeTutor/BridgeTutor/BridgeTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgeTutorApp.swift
+//  BridgeTutor
+//
+//  Created by Marek Forys on 28/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BridgeTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BridgeTutor/BridgeTutor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BridgeTutor
+//
+//  Created by Marek Forys on 28/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Bridge Tutor")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
